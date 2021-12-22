fdceb9b38e557068f83d8bce2d74baf80d152aac
Ilya <ilya.voloshin.16@mail.ru>
2021-12-22 17:05:52 +0500

    Pr. 8

--- Practise №8/main_prog.py
+++ Practise №8/main_prog.py
@@ -3,6 +3,30 @@ from sorts import *
 import time
 from prettytable import PrettyTable
 
+"""
+В данной программе:
+b - обозначение сортировки методом пузырька (bubble_sort)
+s - обозначение сортировки выбором (selection_sort)
+q - обозначение быстрой сортировки (quick_sort)
+"""
+
+
+def sorting(some_list):  # функция возвращающая время сортировки тремя способами и сами отсортированные послед-сти
+    start_time = time.time()
+    b_sort = bubble_sort(some_list)  # сортируем последовательность методом пузырька
+    time_b_sort = time.time() - start_time  # запоминаем время сортировки в соответствующую переменную
+
+    start_time = time.time()
+    s_sort = selection_sort(List)  # сортировка выбором
+    time_s_sort = time.time() - start_time
+
+    start_time = time.time()
+    q_sort = quick_sort(List)  # Быстрая сортировка
+    time_q_sort = time.time() - start_time
+
+    return time_b_sort, time_s_sort, time_q_sort, b_sort, s_sort, q_sort
+
+
 N = int(input("Введите количество элементов последовательности: "))
 
 # Создаём список со случайными целыми числами:
@@ -10,69 +34,49 @@ List = []
 for i in range(N):
     List.append(randint(0, 9))
 
-print(Check(List))  # проверяю отсортирован ли он
+print(Check(List))  # проверяю отсортирована ли последовательность до работы с ней
 
-# Первый способ (сортировка методом пузырька):
-start_time = time.time()
-B_sort_random = bubble_sort(List)  # сортируем случайную последовательность
-time_B_sort_random = time.time() - start_time  # запоминаем время сортировки в соответствующую переменную
+working_process = sorting(List)  # сортирую СЛУЧАЙНУЮ ПОСЛЕДОВАТЕЛЬНОСТЬ с помощью функции и записываю все результаты
 
-start_time = time.time()
-B_Sorted = bubble_sort(B_sort_random)
-time_B_Sorted = time.time() - start_time
+print(Check(working_process[3]))  # проверяю правильность результата сортировки для каждого метода
+print(Check(working_process[4]))
+print(Check(working_process[5]))
 
-B_sort_opposite_way = B_sort_random[::-1]
+# записываю время выполнения сортировки случайной последовательности для каждого метода:
+time_B_sort_random = working_process[0]
+time_S_sort_random = working_process[1]
+time_Q_sort_random = working_process[2]
 
-start_time = time.time()
-B_Sorted_opp_way = bubble_sort(B_sort_opposite_way)
-time_B_Sorted_opp_way = time.time() - start_time
+working_process = sorting(working_process[3])  # сортирую УЖЕ ОСТОРТИРОВАННУЮ ПОСЛЕДОВАТЕЛЬНОСТЬ с помощью фукнции
 
-print(Check(B_sort_random))  # Проверяем отсортировалась ли последовательность
+print(Check(working_process[3]))  # проверяю правильность результата сортировки для каждого метода
+print(Check(working_process[4]))
+print(Check(working_process[5]))
 
+# записываю время выполнения сортировки уже отсортированной последовательности для каждого метода:
+time_B_sorted = working_process[0]
+time_S_sorted = working_process[1]
+time_Q_sorted = working_process[2]
 
-# Второй способ (сортировка выбором):
-start_time = time.time()
-S_sort_random = selection_sort(List)
-time_S_sort_random = time.time() - start_time
+working_process = sorting(working_process[3][::-1])  # сортирую последовательность ОТСОРТИРОВАННУЮ В ОБР. ПОРЯДКЕ
 
-start_time = time.time()
-S_Sorted = selection_sort(B_sort_random)
-time_S_Sorted = time.time() - start_time
+print(Check(working_process[3]))  # проверяю правильность результата сортировки для каждого метода
+print(Check(working_process[4]))
+print(Check(working_process[5]))
 
-S_sort_opposite_way = S_sort_random[::-1]
-
-start_time = time.time()
-S_Sorted_opp_way = selection_sort(S_sort_opposite_way)
-time_S_Sorted_opp_way = time.time() - start_time
-
-print(Check(S_Sorted))
-
-
-# Третий способ (Быстрая сортировка):
-start_time = time.time()
-Q_sort_random = quick_sort(List, 0, len(List)-1)
-time_Q_sort_random = time.time() - start_time
-
-start_time = time.time()
-Q_Sorted = quick_sort(Q_sort_random, 0, len(List)-1)
-time_Q_Sorted = time.time() - start_time
-
-Q_sort_opposite_way = Q_sort_random[::-1]
-
-start_time = time.time()
-Q_Sorted_opp_way = quick_sort(Q_sort_opposite_way, 0, len(List)-1)
-time_Q_Sorted_opp_way = time.time() - start_time
-
-print(Check(Q_sort_random))
+# записываю время выполнения сортировки уже отсортированной последовательности для каждого метода:
+time_B_sorted_opp_way = working_process[0]
+time_S_sorted_opp_way = working_process[1]
+time_Q_sorted_opp_way = working_process[2]
 
 
 # Формируем таблицу с результатами:
 
 result_table = PrettyTable()
 result_table.field_names = ["Методы", "Отсортированная", "Случайная", "Отс. в обратном порядке"]
-result_table.add_row(["Метод пузырька", time_B_sort_random, time_B_Sorted, time_B_Sorted_opp_way])
-result_table.add_row(["Метод пузырька", time_S_sort_random, time_S_Sorted, time_S_Sorted_opp_way])
-result_table.add_row(["Метод пузырька", time_Q_sort_random, time_Q_Sorted, time_Q_Sorted_opp_way])
+result_table.add_row(["Метод пузырька", time_B_sorted, time_B_sort_random, time_B_sorted_opp_way])
+result_table.add_row(["Сортировка выбором", time_S_sorted, time_S_sort_random, time_S_sorted_opp_way])
+result_table.add_row(["Быстрая сортировка", time_Q_sorted, time_Q_sort_random, time_Q_sorted_opp_way])
 
 # Выводим полученную таблицу в файл output.txt:
 
--- Practise №8/sorts.py
+++ Practise №8/sorts.py
@@ -1,9 +1,10 @@
-def bubble_sort(A):
+def bubble_sort(A):  # сортировка методом пузырька
     B = A
     N = len(B)
     R = N
     WasSwap = True
-    while R > 1 and WasSwap:
+    while R > 1 and WasSwap:  # перестанем заходить в цикл либо когда пройдём все элементы послед-сти, либо когда
+                              # пройдёмся по части послед-сти с которой ещё не работали и при этом ничего в ней не поменяем
         WasSwap = False
         for j in range(0, R-1):
             if B[j]>B[j+1]:
@@ -13,7 +14,7 @@ def bubble_sort(A):
     return B    
 
 
-def selection_sort(A):
+def selection_sort(A):  # сортировка выбором
     B = A
     N = len(B)
     for i in range(0,N-1):
@@ -23,20 +24,24 @@ def selection_sort(A):
             if B[j] < min:
                 k = j
                 min = B[j]
-        B[k] = B[i]
-        B[i] = min
+        B[k] = B[i]  # передаем переменной B[k] значение переменной B[i]
+        B[i] = min   # а переменной B[i] передаем значение переменной B[k], таким образом меняя их местами
     return B        
 
 
-def quick_sort(A, L, R):
+def quick_sort(A):  # Быстрая сортировка
     B = A
-    i = L 
-    j = R
-    Pivot = B[L]
-    
+    i = 0  # индекс первого символа последовательности
+    j = len(B)-1  # индекс последнего символа последовательности
+    L = i   # запоминаем индексы начала и конца последовательности в переменные L и R
+    R = j
+    Pivot = B[0]  # опорный элемент
+
     while i < j:
-        while B[i] < Pivot: i += 1
-        while Pivot < B[j]: j -= 1
+        while B[i] < Pivot:
+            i += 1
+        while Pivot < B[j]:
+            j -= 1
         if i <= j:
             if B[i] > B[j]:
                 B[i], B[j] = B[j], B[i]
@@ -44,9 +49,9 @@ def quick_sort(A, L, R):
             j -= 1
     
     if L < j:
-        quick_sort(B, L, j)
+        quick_sort(B[L:j+1])
     if i < R:
-        quick_sort(B, i, R)
+        quick_sort(B[i:R+1])
 
     return B    
 
