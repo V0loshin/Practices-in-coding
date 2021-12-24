0de4af07a6ca23b0e619f94fac97df159fa4a8c2
Ilya <ilya.voloshin.16@mail.ru>
2021-12-24 12:15:45 +0500

    changes in 8

--- Practise №8/sorts.py
+++ Practise №8/sorts.py
@@ -1,66 +1,67 @@
-def bubble_sort(A):  # сортировка методом пузырька
-    B = A
-    N = len(B)
-    R = N
-    WasSwap = True
-    while R > 1 and WasSwap:  # перестанем заходить в цикл либо когда пройдём все элементы послед-сти, либо когда
+# a - список
+def bubble_sort(a):  # сортировка методом пузырька
+    b = a
+    n = len(b)
+    r = n
+    was_swap = True
+    while r > 1 and was_swap:  # перестанем заходить в цикл либо когда пройдём все элементы послед-сти, либо когда
                               # пройдёмся по части послед-сти с которой ещё не работали и при этом ничего в ней не поменяем
-        WasSwap = False
-        for j in range(0, R-1):
-            if B[j]>B[j+1]:
-                B[j], B[j+1] = B[j+1], B[j]
-                WasSwap = True
-        R = R - 1
-    return B    
+        was_swap = False
+        for j in range(0, r-1):
+            if b[j] > b[j+1]:
+                b[j], b[j+1] = b[j+1], b[j]
+                was_swap = True
+        r = r - 1
+    return b
 
 
-def selection_sort(A):  # сортировка выбором
-    B = A
-    N = len(B)
-    for i in range(0,N-1):
-        min = B[i]
+def selection_sort(a):  # сортировка выбором
+    b = a
+    n = len(b)
+    for i in range(0, n-1):
+        min_number = b[i]
         k = i
-        for j in range(i+1,N):
-            if B[j] < min:
+        for j in range(i+1,n):
+            if b[j] < min:
                 k = j
-                min = B[j]
-        B[k] = B[i]  # передаем переменной B[k] значение переменной B[i]
-        B[i] = min   # а переменной B[i] передаем значение переменной B[k], таким образом меняя их местами
-    return B        
+                min_number = b[j]
+        b[k] = b[i]  # передаем переменной B[k] значение переменной B[i]
+        b[i] = min_number   # а переменной B[i] передаем значение переменной B[k], таким образом меняя их местами
+    return b
 
 
-def quick_sort(A):  # Быстрая сортировка
-    B = A
+def quick_sort(a):  # Быстрая сортировка
+    b = a
     i = 0  # индекс первого символа последовательности
-    j = len(B)-1  # индекс последнего символа последовательности
-    L = i   # запоминаем индексы начала и конца последовательности в переменные L и R
-    R = j
-    Pivot = B[0]  # опорный элемент
+    j = len(b)-1  # индекс последнего символа последовательности
+    l = i   # запоминаем индексы начала и конца последовательности в переменные L и R
+    r = j
+    pivot = B[0]  # опорный элемент
 
     while i < j:
-        while B[i] < Pivot:
+        while b[i] < pivot:
             i += 1
-        while Pivot < B[j]:
+        while pivot < b[j]:
             j -= 1
         if i <= j:
-            if B[i] > B[j]:
-                B[i], B[j] = B[j], B[i]
+            if b[i] > b[j]:
+                b[i], b[j] = b[j], b[i]
             i += 1
             j -= 1
-    if L < j:
-        quick_sort(B[L:j+1])
-    if i < R:
-        quick_sort(B[i:R+1])
+    if l < j:
+        quick_sort(b[l:j+1])
+    if i < r:
+        quick_sort(b[i:r+1])
 
-    return B    
+    return b
 
 
-def Check(A):
-    Is_Sorted = True
-    for i in range(len(A)-1):
-        if A[i] <= A[i+1]:
+def check(a):
+    is_sorted = True
+    for i in range(len(a)-1):
+        if a[i] <= a[i+1]:
             continue
         else:
-            Is_Sorted = False
+            is_sorted = False
             break
-    return Is_Sorted
+    return is_sorted
